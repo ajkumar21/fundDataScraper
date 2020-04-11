55684e4120f495337feb9ffdee3b8e968b3a661f
AJ <ajanthan29894@gmail.com>
2020-04-11 08:23:54 +0100

    changed port number

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ app.use(
 
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
 
 app.get("/fund", async (req, res) => {
