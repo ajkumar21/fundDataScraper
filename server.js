77f11c81ccee50559b4f4b42dcb21714a92b4618
AJ <ajanthan29894@gmail.com>
2020-04-13 10:39:19 +0100

    added 404

--- server.js
+++ server.js
@@ -2,6 +2,34 @@ const bodyParser = require("body-parser");
 var express = require("express");
 var scraper = require("./scraper");
 var app = express();
+const MongoClient = require("mongodb").MongoClient;
+
+const connectionString =
+  "mongodb+srv://admin:admin@fundmanagerdata-zei2j.mongodb.net/test?retryWrites=true&w=majority";
+
+MongoClient.connect(connectionString, {
+  useUnifiedTopology: true,
+})
+  .then((client) => {
+    console.log("Connect to mongoDB");
+    const db = client.db("fundData");
+    const fundCollection = db.collection("funds");
+
+    app.post("/fundDB", (req, res) => {
+      fundCollection
+        .insertOne(req.body.link)
+        .then((result) => console.log(result))
+        .catch((err) => console.error(err));
+    });
+
+    app.delete("/fundDB", (req, res) => {
+      fundCollection
+        .deleteOne(req.body.link)
+        .then((result) => console.log(result))
+        .catch((err) => console.error(err));
+    });
+  })
+  .catch((err) => console.error(err));
 
 app.use(
   bodyParser.urlencoded({
@@ -27,10 +55,18 @@ var server = app.listen(process.env.PORT || 8080, function () {
 
 app.get("/fund", async (req, res) => {
   const fund = await scraper.getFundData(req.headers.link);
-  res.status(200).send(fund);
+  if (fund.data.name) {
+    res.status(200).send(fund);
+  } else {
+    res.status(404).send("Not Found");
+  }
 });
 
 app.get("/stock", async (req, res) => {
   const stock = await scraper.getStockData(req.headers.link);
-  res.status(200).send(stock);
+  if (stock.data.name) {
+    res.status(200).send(stock);
+  } else {
+    res.status(404).send("Not Found");
+  }
 });
